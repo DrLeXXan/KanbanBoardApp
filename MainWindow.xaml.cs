c900841b1cccb5b3baf8a74e3edbc460cd18e69f
DrLeXXan <lennart.boehm98@gmail.com>
2025-06-06 12:54:16 +0400

    Rename column header

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,5 +26,49 @@ namespace KanbanBoardApp
             CardDialog cardDialog = new CardDialog();
             cardDialog.ShowDialog();
         }
+
+        private void ColumnTitle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (sender is TextBlock tb && tb.DataContext is KanbanBoardApp.Models.KanbanColumn column)
+                {
+                    column.IsEditing = true;
+                }
+            }
+        }
+
+        private void ColumnTitleTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (sender is TextBox tb && tb.DataContext is KanbanBoardApp.Models.KanbanColumn column)
+                {
+                    column.IsEditing = false;
+                }
+            }
+        }
+
+        private void Window_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // If the click is inside a TextBox, do nothing (let editing continue)
+            if (e.OriginalSource is DependencyObject depObj)
+            {
+                var parent = depObj;
+                while (parent != null)
+                {
+                    if (parent is TextBox)
+                        return;
+                    parent = VisualTreeHelper.GetParent(parent);
+                }
+            }
+
+            // Otherwise, set IsEditing = false for all columns
+            if (DataContext is KanbanBoardApp.ViewModels.MainViewModel vm)
+            {
+                foreach (var col in vm.Columns)
+                    col.IsEditing = false;
+            }
+        }
     }
 }
--- Models/KanbanColumn.cs
+++ Models/KanbanColumn.cs
@@ -1,15 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KanbanBoardApp.Models
 {
-    public class KanbanColumn
+    public class KanbanColumn : INotifyPropertyChanged
     {
-        public string Title { get; set; }
-        //public ObservableCollection<KanbanCard> Cards { get; set; } = new();
+        private string _title = string.Empty;
+        private bool _isEditing;
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    OnPropertyChanged(nameof(Title));
+                }
+            }
+        }
+
+        public bool IsEditing
+        {
+            get => _isEditing;
+            set
+            {
+                if (_isEditing != value)
+                {
+                    _isEditing = value;
+                    OnPropertyChanged(nameof(IsEditing));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    //public ObservableCollection<KanbanCard> Cards { get; set; } = new();
     }
 }
